4bfe14739a93412ecb0cd582d49b05a426d9d12b
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-06-21 14:44:11 +0200

    interpolation on arrays

--- book-rating/src/app/dashboard/dashboard.component.ts
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  books: string[];
 
   constructor() { }
 
   ngOnInit() {
+    this.books = ['Angular', 'AngularJS 1.x']
   }
-
 }
