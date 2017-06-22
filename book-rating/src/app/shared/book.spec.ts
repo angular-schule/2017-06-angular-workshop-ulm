2e239df778ef4f6059edff9b0dc2e53c75635eba
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-06-22 09:57:08 +0200

    isolated unit tests

--- book-rating/src/app/shared/book.spec.ts
+++ book-rating/src/app/shared/book.spec.ts
@@ -1,7 +1,34 @@
-import {Book} from './book';
+import { Book } from './book';
 
 describe('Book', () => {
-  it('should create an instance', () => {
-    expect(new Book('', '', '')).toBeTruthy();
+
+  let book: Book;
+
+  beforeEach(() => {
+    book = new Book('', '', '');
+  })
+
+  it('should rate up by 1', () => {
+    book.rating = 0;
+    book.rateUp();
+    expect(book.rating).toBe(1);
+  });
+
+  it('should rate down by 1', () => {
+    book.rating = 1;
+    book.rateDown();
+    expect(book.rating).toBe(0);
+  });
+
+  it('should not be allowed to have a rating greater than 5', () => {
+    book.rating = 5;
+    book.rateUp();
+    expect(book.rating).toBe(5);
+  });
+
+  it('should not be possible to have a rating smaller than 0', () => {
+    book.rating = 0;
+    book.rateDown();
+    expect(book.rating).toBe(0);
   });
 });
